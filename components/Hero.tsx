97d5c5c9871296c2871e7bf3f2453deb848d1844
waelbaraka <pytsud@gmail.com>
2024-10-10 21:51:12 +0400

    padding hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -6,8 +6,8 @@ import { TextGenerateEffect } from "./ui/TextGenerateEffect";
 
 const Hero = () => {
   return (
-    <div className="pb-20 pt-36">
-      {/**
+    <div className="pb-20 ">
+      {/**  pt-36
        *  UI: Spotlights
        *  Link: https://ui.aceternity.com/components/spotlight
        */}
