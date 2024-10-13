a8623d3ad4cf051046eae60d7f45b2f585a9198d
waelbaraka <pytsud@gmail.com>
2024-10-13 04:31:25 -0700

    footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -17,8 +17,7 @@ const Footer = () => {
 
       <div className="flex flex-col items-center">
         <h1 className="heading lg:max-w-[45vw]">
-          Ready to take <span className="text-purple">your</span> Business
-          to run on Auto Pilot?
+          Ready to let your Business <span className="text-purple">flow</span> smoothly and operate on autopilot?
         </h1>
         <p className="text-white-200 md:mt-10 my-5 text-center">
           Reach out to us today and let&apos;s discuss how  can help you
